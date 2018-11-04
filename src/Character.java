c3f8ab8db81d0fa241bd4cdd1fbf32c1e406b064
Mike <mblack101@ufl.edu>
2018-11-04 02:23:48 -0500

    added comments to Character interface

--- src/Character.java
+++ src/Character.java
@@ -1,7 +1,27 @@
+/**
+ * Characters can either be PlayerCharacters (PCs) or NonPlayerCharacters (NPCs). In this game, the NPCs are monsters
+ * that will be generated for the PC, controlled by the player, to fight.
+ */
 interface Character {
 
+  /**
+   * All characters have the ability to attack and cause damage to another character. The amount of damage is
+   * determined by a formula that factors the weapon used.
+   * @return damage - int amount of damage caused during the attack
+   */
   int attack();
-  int defend();
-  void printStats();
 
+  /**
+   * All characters can defend against attacks from other characters. Depending on their armor, they can block a
+   * portion of the attack or cause the attacker to miss entirely.
+   *
+   * @return defense - int amount to reduce the damage caused by an attacker
+   */
+  int defend();
+
+  /**
+   * Print a list of statistics for the character. This is similar to "toString()" except only select fields
+   * are returned.
+   */
+  void printStats();
 }
