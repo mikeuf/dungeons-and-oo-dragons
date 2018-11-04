8e6a90a25e9fa4c59e722db4e87fa4d4a74c04d2
Mike <mblack101@ufl.edu>
2018-11-04 03:52:23 -0500

    updated JavaDocs for GameOptions

--- src/GameStatus.java
+++ src/GameStatus.java
@@ -1,3 +1,8 @@
+/**
+ * GameStatus.java
+ *
+ * Tracks whether the PlayerCharacter has won or lost the battle, and whether the game is over.
+ */
 public class GameStatus {
 
   protected enum CurrentGameStatus {
